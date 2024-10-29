00ccfb9af91ec0441c5375cf6ed48515f9c5736d
dhars <dhars@cisco.com>
2024-10-30 01:30:35 +0530

    entity update:

--- entitites/database.py
+++ entitites/database.py
@@ -1,9 +1,13 @@
 from entitites.tables import Table
-
+from entitites.columns import Column
 class Database:
     
     def __init__(self) -> None:
         self.tables=[]
         
-    def add_table(self,name,columns):
+    def create_table(self,name,columns):
         table = Table(name)
+        for name, data_type, constraints in columns:
+            table.add_column(name, data_type, constraints)
+        self.tables[name]=table
+            
